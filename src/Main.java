67e06766e549145af3e34a6d42c053e305cc009a
Tarık <tarqkaksu@gmail.com>
2021-09-27 00:29:06 +0300

    Inital commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int age = 25 ;
+
+        String name = "Tarik" ;
+
+        double number = 29.8 ;
+
+        boolean value = false ;
+
+
+
+        if (age == 25 && name == "Tarik"){
+
+            System.out.println("Kod bloğuna girdi");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
